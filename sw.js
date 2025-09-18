4d81d4634a0c33d78dc80ae33c925049cfe7cab5
Tevi74 <yeyalola126@gmail.com>
2025-09-18 12:49:14 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'sim-vein-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './sim/engine.js',
+  './ia/tutor.js',
+  './public/imagens/logo-sim.png',
+  './public/imagens/logo-instituto.png',
+  './public/imagens/logo-harastech.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
